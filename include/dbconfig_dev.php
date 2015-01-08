7e60be4eacaa296debe21ad7adae9fb4b9190116
soar <soar.lin@gmail.com>
2015-01-08 15:18:18 +0800

    [Add]stores_db operator and dbconnect by server_name

--- include/dbconfig_dev.php
+++ include/dbconfig_dev.php
@@ -3,12 +3,21 @@
 
     function getDevDBConnection(){
         global $devDB;
+        $server_url = $_SERVER['SERVER_NAME'];
+        if ($server_url == "soar.eatme.tw") {
+            $db_host = 'eatmedb.cgcvau6esuvd.ap-northeast-1.rds.amazonaws.com';
+            $db_name = 'cashflow';
+            $db_user = 'eatmedev';
+            $db_pass = 'eatmedev';
+        } else if ($server_url == "localhost") {
+            $db_host = '127.0.0.1';
+            $db_name = 'test0401';
+            $db_user = 'root';
+            $db_pass = 'mysql';
+        }
         $db_type = 'mysql';
-        $db_host = 'eatmedb.cgcvau6esuvd.ap-northeast-1.rds.amazonaws.com';
-        $db_name = 'cashflow';
-        $db_user = 'eatmedev';
-        $db_pass = 'eatmedev';
         $connect_host = $db_type . ':host=' . $db_host . ';dbname=' . $db_name;
+
         try{
             if(!$devDB){
                 $devDB = new PDO($connect_host, $db_user, $db_pass);
